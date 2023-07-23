acf6affa8c9a4caf4db4acbb6e24e7b9289c15e3
Kim <ddino99@naver.com>
2023-07-23 15:35:20 +0900

    initial commit

---
+++ ServerCore/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ServerCore
+{
+    internal class Program
+    {
+        static object _obj = new object();
+        static void Main(string[] args)
+        {
+            lock (_obj)
+            {
+
+            }
+            
+        }
+    }
+}
