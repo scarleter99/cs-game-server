45c43c4950f9b1b754881dac07b2800202af0364
Kim <ddino99@naver.com>
2023-08-01 17:47:06 +0900

    Initial commit

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -4,16 +4,57 @@ using System.Threading.Tasks;
 
 namespace ServerCore
 {
-    internal class Program
+    class Lock
     {
-        static object _obj = new object();
+        AutoResetEvent _available = new AutoResetEvent(true);
+
+        public void Acquire()
+        {
+            _available.WaitOne();
+        }
+
+        public void Release()
+        {
+            _available.Set();
+        }
+
+    }
+
+    class Program
+    {
+        static int _num = 0;
+        static Lock _lock = new Lock();
+
+        static void Thread_1()
+        {
+            for (int i = 0; i < 10000; i++)
+            {
+                _lock.Acquire();
+                _num++;
+                _lock.Release();
+            }
+        }
+
+        static void Thread_2()
+        {
+            for (int i = 0; i < 10000; i++)
+            {
+                _lock.Acquire();
+                _num--;
+                _lock.Release();
+            }
+        }
+
         static void Main(string[] args)
         {
-            lock (_obj)
-            {
+            Task t1 = new Task(Thread_1);
+            Task t2 = new Task(Thread_2);
+            t1.Start();
+            t2.Start();
 
-            }
-            
+            Task.WaitAll(t1, t2);
+
+            Console.WriteLine(_num);
         }
     }
 }
