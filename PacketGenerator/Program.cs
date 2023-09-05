0c12f962714f40c3a2b13e051094384eee741046
Kim Taebum <ddino99@naver.com>
2023-09-05 18:25:22 +0900

    Packet Generator #4

---
+++ PacketGenerator/Program.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+
+namespace PacketGenerator
+{
+    class Program
+    {
+        static string genPackets; // GenPackets.cs에 파싱할 코드
+        static ushort packetId; // 패킷 번호
+        static string packetEnums; // 패킷 Enum을 파싱한 코드
+
+        static string clientRegister; // ClientPacketManager.cs에 파싱할 코드
+        static string serverRegister; // ServerPacketManager.cs에 파싱할 코드
+
+        static void Main(string[] args)
+        {
+            string pdlPath = "../../PDL.xml";
+
+            XmlReaderSettings settings = new XmlReaderSettings()
+            {
+                // 주석, 공백 무시
+                IgnoreComments = true,
+                IgnoreWhitespace = true
+            };
+
+            if (args.Length >= 1)
+                pdlPath = args[0];
+
+            using (XmlReader r = XmlReader.Create(pdlPath, settings))
+            {
+                // 헤더 패스
+                r.MoveToContent();
+
+                while (r.Read())
+                {
+                    // 깊이가 1인 시작 노드일 때 패킷 파싱
+                    if (r.Depth == 1 && r.NodeType == XmlNodeType.Element)
+                        ParsePacket(r);
+                    //Console.WriteLine(r.Name + " " + r["name"]);
+                }
+
+                // GetPackets.cs, ClientPacketManager.cs, ServerPacketManager.cs 파일 생성
+                string fileText = string.Format(PacketFormat.fileFormat, packetEnums, genPackets);
+                File.WriteAllText("GenPackets.cs", fileText);
+                string clientManagerText = string.Format(PacketFormat.managerFormat, clientRegister);
+                File.WriteAllText("ClientPacketManager.cs", clientManagerText);
+                string serverManagerText = string.Format(PacketFormat.managerFormat, serverRegister);
+                File.WriteAllText("ServerPacketManager.cs", serverManagerText);
+            }
+        }
+
+        // 패킷 파싱
+        public static void ParsePacket(XmlReader r)
+        {
+            if (r.NodeType == XmlNodeType.EndElement)
+                return;
+
+            if (r.Name.ToLower() != "packet")
+            {
+                Console.WriteLine("Invalid packet node");
+                return;
+            }
+
+            string packetName = r["name"];
+            if (string.IsNullOrEmpty(packetName))
+            {
+                Console.WriteLine("Packet without name");
+                return;
+            }
+
+            // 멤버 변수, Write(), Read() 파싱
+            Tuple<string, string, string> t = ParseMembers(r);
+            // 패킷, 패킷 Enum 파싱
+            genPackets += string.Format(PacketFormat.packetFormat, packetName, t.Item1, t.Item2, t.Item3);
+            packetEnums += string.Format(PacketFormat.packetEnumFormat, packetName, ++packetId) + Environment.NewLine + "\t"; // 줄바꿈
+
+            // 서버/클라 패킷 구분
+            if (packetName.StartsWith("S_") || packetName.StartsWith("s_"))
+                clientRegister += string.Format(PacketFormat.managerRegisterFormat, packetName) + Environment.NewLine;
+            else
+                serverRegister += string.Format(PacketFormat.managerRegisterFormat, packetName) + Environment.NewLine;
+        }
+
+        // 멤버 변수 파싱
+        // {1} 멤버 변수들
+        // {2} 멤버 변수 Read
+        // {3} 멤버 변수 Write
+        public static Tuple<string, string, string> ParseMembers(XmlReader r)
+        {
+            string packetName = r["name"];
+
+            string memberCode = "";
+            string readCode = "";
+            string writeCode = "";
+
+            int depth = r.Depth + 1;
+            while (r.Read())
+            {
+                if (r.Depth != depth)
+                    break;
+
+                string memberName = r["name"];
+                if (string.IsNullOrEmpty(memberName))
+                {
+                    Console.WriteLine("Member without name");
+                    return null;
+                }
+
+                // 줄바꿈
+                if (string.IsNullOrEmpty(memberCode) == false)
+                    memberCode += Environment.NewLine;
+                if (string.IsNullOrEmpty(readCode) == false)
+                    readCode += Environment.NewLine;
+                if (string.IsNullOrEmpty(writeCode) == false)
+                    writeCode += Environment.NewLine;
+
+                // 멤버 타입 별로 멤버영역, Read(), Write()에 추가할 코드 파싱
+                string memberType = r.Name.ToLower();
+                switch (memberType)
+                {
+                    case "byte":
+                    case "sbyte":
+                        memberCode += string.Format(PacketFormat.memberFormat, memberType, memberName);
+                        readCode += string.Format(PacketFormat.readByteFormat, memberName, memberType);
+                        writeCode += string.Format(PacketFormat.writeByteFormat, memberName, memberType);
+                        break;
+                    case "bool":
+                    case "short":
+                    case "ushort":
+                    case "int":
+                    case "long":
+                    case "float":
+                    case "double":
+                        memberCode += string.Format(PacketFormat.memberFormat, memberType, memberName);
+                        readCode += string.Format(PacketFormat.readFormat, memberName, ToMemberType(memberType), memberType);
+                        writeCode += string.Format(PacketFormat.writeFormat, memberName, memberType);
+                        break;
+                    case "string":
+                        memberCode += string.Format(PacketFormat.memberFormat, memberType, memberName);
+                        readCode += string.Format(PacketFormat.readStringFormat, memberName);
+                        writeCode += string.Format(PacketFormat.writeStringFormat, memberName);
+                        break;
+                    case "list":
+                        Tuple<string, string, string> t = ParseList(r);
+                        memberCode += t.Item1;
+                        readCode += t.Item2;
+                        writeCode += t.Item3;
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            // 코드 정렬
+            memberCode = memberCode.Replace("\n", "\n\t");
+            readCode = readCode.Replace("\n", "\n\t\t");
+            writeCode = writeCode.Replace("\n", "\n\t\t");
+            return new Tuple<string, string, string>(memberCode, readCode, writeCode);
+        }
+
+        // List 멤버 변수 파싱
+        public static Tuple<string, string, string> ParseList(XmlReader r)
+        {
+            string listName = r["name"];
+            if (string.IsNullOrEmpty(listName))
+            {
+                Console.WriteLine("List without name");
+                return null;
+            }
+
+            // 멤버영역, Read(), Write() 부분은 기존 멤버 변수와 동일
+            Tuple<string, string, string> t = ParseMembers(r);
+
+            string memberCode = string.Format(PacketFormat.memberListFormat,
+                FirstCharToUpper(listName),
+                FirstCharToLower(listName),
+                t.Item1,
+                t.Item2,
+                t.Item3);
+
+            string readCode = string.Format(PacketFormat.readListFormat,
+                FirstCharToUpper(listName),
+                FirstCharToLower(listName));
+
+            string writeCode = string.Format(PacketFormat.writeListFormat,
+                FirstCharToUpper(listName),
+                FirstCharToLower(listName));
+
+            return new Tuple<string, string, string>(memberCode, readCode, writeCode);
+        }
+
+        // Type을 Type변환 함수로
+        public static string ToMemberType(string memberType)
+        {
+            switch (memberType)
+            {
+                case "bool":
+                    return "ToBoolean";
+                case "short":
+                    return "ToInt16";
+                case "ushort":
+                    return "ToUInt16";
+                case "int":
+                    return "ToInt32";
+                case "long":
+                    return "ToInt64";
+                case "float":
+                    return "ToSingle";
+                case "double":
+                    return "ToDouble";
+                default:
+                    return "";
+            }
+        }
+
+        // 첫 문자를 대문자로
+        public static string FirstCharToUpper(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+                return "";
+            return input[0].ToString().ToUpper() + input.Substring(1);
+        }
+
+        // 첫 문자를 소문자로
+        public static string FirstCharToLower(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+                return "";
+            return input[0].ToString().ToLower() + input.Substring(1);
+        }
+    }
+}
